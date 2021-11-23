a144a639a70d050809e6ef187ee7d7792ac6bc55
ss251 <ss251@uw.edu>
2021-11-23 17:30:35 -0600

    initial commit

---
+++ app/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+// Component
+// State
+// Lifecycle
+// UI
+
+class App extends React.Component {
+  render() {
+    return <div>Time for a change!</div>;
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
