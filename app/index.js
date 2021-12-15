f4916b198daa144097c09887b51d63aa09e0bf56
ss251 <ss251@uw.edu>
2021-12-15 10:55:40 -0600

    add theme toggling

--- app/index.js
+++ app/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import Popular from "./components/Popular";
 import Battle from "./components/Battle";
+import { ThemeProvider } from "./contexts/theme";
+import Nav from "./components/Nav";
 
 // Component
 // State
@@ -10,11 +12,29 @@ import Battle from "./components/Battle";
 // UI
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      theme: "light",
+      toggleTheme: () => {
+        this.setState(({ theme }) => ({
+          theme: theme === "light" ? "dark" : "light",
+        }));
+      },
+    };
+  }
   render() {
     return (
-      <div className="container">
-        <Battle />
-      </div>
+      <ThemeProvider value={this.state}>
+        <div className={this.state.theme}>
+          <div className="container">
+            <Nav />
+
+            <Battle />
+          </div>
+        </div>
+      </ThemeProvider>
     );
   }
 }
